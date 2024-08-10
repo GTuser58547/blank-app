429a57539da61fbbfc24d815783c283bf0f3a827
GTuser58547 <121080215+GTuser58547@users.noreply.github.com>
2024-08-10 11:23:09 +0800

    Update streamlit_app.py
    添加了文本和链接

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).（左边这儿是开发者看的。）"
 )
+st.write("下面这些怎么样？")
 st.markdown("一个可能用得上的工具网站[你可以点一下](https://www.jyshare.com)")
 st.markdown("不知道你想不想玩玩小游戏[点这里](https://www.yikm.net)")
+st.markdown("或者自己做个小游戏也行[确实](https://www.construct.net/en)")
