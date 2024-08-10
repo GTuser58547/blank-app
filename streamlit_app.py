3029ad57a346dca9449870a7c6e9c6b1ec1a6ba5
GTuser58547 <121080215+GTuser58547@users.noreply.github.com>
2024-08-10 11:16:50 +0800

    Update streamlit_app.py
    添加了一个链接

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.markdown("一个可能用得上的工具网站[你可以点一下](https://www.jyshare.com)")
+st.markdown("不知道你想不想玩玩小游戏[点这里](https://www.yikm.net)")
